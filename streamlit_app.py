c953356db41138c2614dac1f9733b1c9fcad2d0f
Ramasamy2002 <sridhartcr5865@gmail.com>
2023-09-06 13:23:47 +0530

    commit

---
+++ streamlit_app.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import tensorflow_hub as hub
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+
+from PIL import Image
+import numpy as np
+st.title("Sea Animals Image Classification")
+
+st.write("Predict the Sea Animal that is being represented in the image.")
+
+model = load_model("C:/Users/91934/Downloads/DLDEPLOYMENT/weathertransfer.h5",custom_objects={'KerasLayer':hub.KerasLayer})
+labels={
+    0:'dew',
+    1:'fogsmog',
+    2:'frost',
+    3:'glaze',
+    4:'hail',
+    5:'lightning',
+    6:'rain',
+    7:'rainbow',
+    8:'rime',
+    9:'sandstrom',
+    10:'snow'
+}
+uploaded_file = st.file_uploader(
+    "Upload an image of a Sea Animal:", type=['jpg','png','jpeg']
+)
+predictions=-1
+if uploaded_file is not None:
+    image1 = Image.open(uploaded_file)
+    image1=image.smart_resize(image1,(256,256))
+    img_array = image.img_to_array(image1)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array/255.0
+    predictions = model.predict(img_array)
+    label=labels[np.argmax(predictions)]
+
+
+st.write("### Prediction Result")
+if st.button("Predict"):
+    if uploaded_file is not None:
+        image1 = Image.open(uploaded_file)
+        st.image(image1, caption="Uploaded Image", use_column_width=True)
+        st.markdown(
+            f"<h2 style='text-align: center;'>Image of {label}</h2>",
+            unsafe_allow_html=True,
+        )
+    else:
+        st.write("Please upload file or choose sample image.")
